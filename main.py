37f021783378bdb8644a65931b54cbe4a9ca2034
Amrito Mohonto Pial <110146632+Amrito20@users.noreply.github.com>
2024-01-05 07:16:04 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import google.generativeai as genai
+from IPython.display import display
+from IPython.display import Markdown
+
+api_key = st.secrets["API_KEY"]
+genai.configure(api_key=api_key)
+model = genai.GenerativeModel('models/gemini-pro')
+
+
+
+if "chat" not in st.session_state:
+    st.session_state.chat = model.start_chat(history=[])
+
+#display tittle
+st.title("Chat With *Gemini-Pro*")
+
+def role_to_streamlit(role):
+    if role == "model":
+        return "assistant"
+    else:
+        return role
+    
+
+for message in st.session_state.chat.history:
+    with st.chat_message(role_to_streamlit(message.role)):
+        st.markdown(message.parts[0].text)
+
+
+if prompt := st.chat_input("ASK ME ANYTHING YOU WANT"):
+    st.chat_message("user").markdown(prompt)
+    response= st.session_state.chat.send_message(prompt)
+    with st.chat_message("assistant"):
+        st.markdown(response.text)
+
+
+        
